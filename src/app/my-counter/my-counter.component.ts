89228238e0a8ccab6f1f1ffd2112f542727192d9
Michael Chao <quafex@gmail.com>
2019-08-01 23:50:19 -0400

    add timer

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,34 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { increment, decrement, reset } from '../counter.actions';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { increment, decrement, reset } from "../counter.actions";
 
 @Component({
-  selector: 'app-my-counter',
-  templateUrl: './my-counter.component.html',
-  styleUrls: ['./my-counter.component.css']
+  selector: "app-my-counter",
+  templateUrl: "./my-counter.component.html",
+  styleUrls: ["./my-counter.component.css"]
 })
 export class MyCounterComponent implements OnInit {
-
   count$: Observable<number>;
- 
+  currentTime: string;
+  myTimer;
+
   constructor(private store: Store<{ count: number }>) {
-    this.count$ = store.pipe(select('count'));
+    this.count$ = store.pipe(select("count"));
+
+    this.myTimer = setInterval(() => {
+      this.currentTime = new Date().toLocaleTimeString();
+    }, 1000);
   }
- 
+
   increment() {
     this.store.dispatch(increment());
   }
- 
+
   decrement() {
     this.store.dispatch(decrement());
   }
- 
+
   reset() {
     this.store.dispatch(reset());
-  } 
-
-  ngOnInit() {
   }
 
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    clearInterval(this.myTimer);
+  }
 }
